07d31114e46f0bca940293999ce28be40a2560f7
Jordan Smalls <smalls.jordan@gmail.com>
2026-01-31 19:35:58 -0500

    chore: update comment for route and desc in redirect.route.js

--- server/src/routes/redirect.route.js
+++ server/src/routes/redirect.route.js
@@ -2,6 +2,8 @@ import e from "express";
 const router = e.Router();
 import { handleRedirect } from "../controllers/redirect.controller.js";
 
+//  @desc    handle short link redirect + analytics tracking
+//  @route   GET /:slug
 router.get("/:slug", handleRedirect);
 
 export default router;
